2423a9fd392da954da0d880b6a4c313613da57dd
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-20 21:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let input = [1, 2, 3, 4];
+      function manipulate(){
+        return new Promise((resolve,reject)=>{
+          let newInput = []
+          for(let i of input){
+            if(i%2==0) newInput.push(i)
+          }
+        setTimeout(()=>{
+          resolve(newInput)
+          let otpt = document.getElementById('output')
+          otpt.innerText=newInput
+        },1000)
+        })
+      }
+      manipulate().then((newInput)=>{
+        setTimeout(()=>{
+          let otpt = document.getElementById('output')
+          otpt.innerText=newInput.map(value=>value*2)
+        },2000)
+        console.log(newInput)
+      })
